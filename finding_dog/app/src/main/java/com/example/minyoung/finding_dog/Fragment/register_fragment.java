880f83e3ed96d460849bb1c03029f033d1af4594
JangMinYoung <min8540055@hanmail.net>
2018-06-28 15:40:51 +0900

    android 디렉토리 -> finding_dog으로 변경 및 어플리케이션 생성

---
+++ finding_dog/app/src/main/java/com/example/minyoung/finding_dog/Fragment/register_fragment.java
@@ -0,0 +1,24 @@
+package com.example.minyoung.finding_dog.Fragment;
+
+import android.app.Fragment;
+import android.os.Bundle;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.minyoung.finding_dog.R;
+
+/**
+ * Created by minyoung on 2018-06-28.
+ */
+
+public class register_fragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_register, container, false);
+    }
+
+
+
+}
---
+++ finding_dog/app/src/main/java/com/example/minyoung/finding_dog/LoginActivity.java
@@ -0,0 +1,48 @@
+package com.example.minyoung.finding_dog;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Build;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.remoteconfig.FirebaseRemoteConfig;
+
+public class LoginActivity extends AppCompatActivity {
+    private Button login;
+    private Button singup;
+    private FirebaseRemoteConfig mFirebaseRemoteConfig;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        mFirebaseRemoteConfig=FirebaseRemoteConfig.getInstance();
+        String splash_background=mFirebaseRemoteConfig.getString("splash_background");
+
+        if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.LOLLIPOP){
+            getWindow().setStatusBarColor(Color.parseColor(splash_background));
+        }
+
+        login=(Button)findViewById(R.id.loginActivity_button_login);
+        singup =(Button)findViewById(R.id.loginActivity_button_signup);
+        login.setBackgroundColor(Color.parseColor(splash_background));
+        singup.setBackgroundColor(Color.parseColor(splash_background));
+        singup.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                startActivity(new Intent(LoginActivity.this,SignupActivity.class));
+            }
+        });
+        login.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                startActivity(new Intent(LoginActivity.this,MainActivity.class));
+            }
+        });
+
+    }
+
+
+}
