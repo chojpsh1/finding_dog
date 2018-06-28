7b53a47d5b4f67b22c3524022923f98b4a80420f
JeonSeongBae <tjdqo6042@naver.com>
2018-06-29 04:23:08 +0900

    Auth 기능 진행 중

--- finding_dog/app/src/main/java/com/example/minyoung/finding_dog/SignupActivity.java
+++ finding_dog/app/src/main/java/com/example/minyoung/finding_dog/SignupActivity.java
@@ -1,8 +1,10 @@
 package com.example.minyoung.finding_dog;
 
+import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -10,21 +12,71 @@ import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class SignupActivity extends AppCompatActivity {
-    private EditText email;
-    private EditText name;
-    private EditText password;
-    private Button signup;
+    final String TAG = "SignupActivity";
+
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+    Context mContext;
+    private EditText editTextEmail;
+    private EditText editTextName;
+    private EditText editTextPassword;
+    private Button buttonSignUp;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+        mContext = this;
 
-        email=(EditText)findViewById(R.id.signupActivity_edittext_email);
-        name=(EditText)findViewById(R.id.signupActivity_edittext_name);
-        password=(EditText)findViewById(R.id.signupActivity_edittext_password);
-        signup=(Button)findViewById(R.id.signupActivity_button_signup);
+        editTextEmail = findViewById(R.id.editTextEmail);
+        editTextName = findViewById(R.id.editTextName);
+        editTextPassword = findViewById(R.id.editTextPassword);
+        buttonSignUp = findViewById(R.id.buttonSignUp);
+        
+        buttonSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                createAccount(editTextEmail.getText().toString(), editTextPassword.getText().toString());
+            }
+        });
+        
+        mAuth = FirebaseAuth.getInstance();
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    // User is signed in
+                    Log.d(TAG, "onAuthStateChanged:signed_in:" + user.getUid());
+                } else {
+                    // User is signed out
+                    Log.d(TAG, "onAuthStateChanged:signed_out");
+                }
+            }
+        };
+    }
 
+    private void createAccount(String email, String password) {
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener() {
+                    @Override
+                    public void onComplete(@NonNull Task task) {
+                        Log.d(TAG, "createUserWithEmail:onComplete:" + task.isSuccessful());
+
+                        // If sign in fails, display a message to the user. If sign in succeeds
+                        // the auth state listener will be notified and logic to handle the
+                        // signed in user can be handled in the listener.
+                        if (!task.isSuccessful()) {
+                            Toast.makeText(mContext, "Authentication failed",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+        // [END create_user_with_email]
     }
 }
