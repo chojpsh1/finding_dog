66dafde06c49b4d1af02dffd895b905403028006
JangMinYoung <min8540055@hanmail.net>
2018-08-15 04:38:22 +0900

    setting 구현 완성,search_fragment UI

--- finding_dog/app/src/main/java/com/example/minyoung/finding_dog/Fragment/setting_fragment.java
+++ finding_dog/app/src/main/java/com/example/minyoung/finding_dog/Fragment/setting_fragment.java
@@ -1,15 +1,20 @@
 package com.example.minyoung.finding_dog.Fragment;
 
 import android.app.Fragment;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.CompoundButton;
+import android.widget.EditText;
 import android.widget.GridView;
 import android.widget.Switch;
 import android.widget.Toast;
 
+import com.example.minyoung.finding_dog.LoginActivity;
+import com.example.minyoung.finding_dog.MainActivity;
 import com.example.minyoung.finding_dog.R;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -28,10 +33,26 @@ import java.util.Map;
 public class setting_fragment extends Fragment{
     DatabaseReference databaseReference;
     String current_uid;
+    EditText user_edit;
+    Button logout_btn;
+    View view;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_setting, container, false);
+        view = inflater.inflate(R.layout.fragment_setting, container, false);
         Switch sw = (Switch)view.findViewById(R.id.switch1);
+        user_edit = (EditText)view.findViewById(R.id.user_edittext);
+        logout_btn= (Button)view.findViewById(R.id.logout_btn);
+
+        logout_btn.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                //Start your activity here
+                Intent i = new Intent(view.getContext(), LoginActivity.class);
+                startActivity(i);
+            }
+        });
 
         DatabaseReference database2 = FirebaseDatabase.getInstance().getReference();
         DatabaseReference mConditionRef2=database2.child("Current_user");
@@ -71,6 +92,28 @@ public class setting_fragment extends Fragment{
 
             }
 
+        });
+
+        DatabaseReference database = FirebaseDatabase.getInstance().getReference();
+        DatabaseReference mConditionRef=database.child("User");
+
+        mConditionRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                Iterator<DataSnapshot> child = dataSnapshot.getChildren().iterator();
+
+                while(child.hasNext()) {
+                    DataSnapshot temp=child.next();
+                    String key=temp.getKey();
+                    if(key.equals(current_uid)){
+                        String str=temp.child("uid").getValue(String.class);
+                        user_edit.setText(str);
+                    }
+                }
+            }
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+            }
         });
         return view;
     }
