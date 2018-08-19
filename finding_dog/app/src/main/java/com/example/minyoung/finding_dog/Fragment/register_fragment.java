745ecec9158ff5f12bdf8771c4e174398cf02771
JangMinYoung <min8540055@hanmail.net>
2018-08-20 01:10:09 +0900

    CLOUD_VISION_API를 통해 나온 종의 강아지정보 리스트 출력

--- finding_dog/app/src/main/java/com/example/minyoung/finding_dog/Fragment/register_fragment.java
+++ finding_dog/app/src/main/java/com/example/minyoung/finding_dog/Fragment/register_fragment.java
@@ -88,7 +88,7 @@ public class register_fragment extends Fragment {
     StorageReference firebaseStorageRef;
     String current_uid;
 
-    private static final String CLOUD_VISION_API_KEY = "AIzaSyC9ucj6c1SJRSzRERLiheBrElqYfU76K_k";
+    private static final String CLOUD_VISION_API_KEY = "";
     private static final String ANDROID_PACKAGE_HEADER = "X-Android-Package";
     private static final String ANDROID_CERT_HEADER = "X-Android-Cert";
     private static final int MAX_LABEL_RESULTS = 5;
