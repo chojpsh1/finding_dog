d9611f57ae40040fdbd58728ebf2665bae311589
JeonSeongBae <tjdqo6042@naver.com>
2018-08-20 17:25:19 +0900

    setting frgment에서 UID 받아오기

--- finding_dog/app/src/main/java/com/example/minyoung/finding_dog/Fragment/setting_fragment.java
+++ finding_dog/app/src/main/java/com/example/minyoung/finding_dog/Fragment/setting_fragment.java
@@ -17,6 +17,7 @@ import com.example.minyoung.finding_dog.LoginActivity;
 import com.example.minyoung.finding_dog.MainActivity;
 import com.example.minyoung.finding_dog.R;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -38,14 +39,18 @@ public class setting_fragment extends Fragment{
     Button logout_btn;
     Switch sw;
     View view;
+    FirebaseAuth mAuth;
+    FirebaseUser user;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.fragment_setting, container, false);
         sw = (Switch) view.findViewById(R.id.switch1);
         user_edit = (EditText) view.findViewById(R.id.user_edittext);
+        mAuth = FirebaseAuth.getInstance();
+        user = mAuth.getCurrentUser();
+        current_uid = user.getUid();
         logout_btn= (Button) view.findViewById(R.id.logout_btn);
-
-
 
         logout_btn.setOnClickListener(new View.OnClickListener()
         {
@@ -54,26 +59,14 @@ public class setting_fragment extends Fragment{
             {
                 //Start your activity here
                 FirebaseAuth.getInstance().signOut();
-                Intent i = new Intent(view.getContext(), LoginActivity.class);
-                startActivity(i);
+                startActivity(new Intent(view.getContext(), LoginActivity.class));
                 finish();
             }
         });
 
         DatabaseReference database2 = FirebaseDatabase.getInstance().getReference();
         DatabaseReference mConditionRef2=database2.child("Current_user");
-        mConditionRef2.addListenerForSingleValueEvent(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                Iterator<DataSnapshot> child = dataSnapshot.getChildren().iterator();
-                DataSnapshot temp=child.next();
-                String key = temp.getKey();
-                current_uid = key;
-            }
-            @Override
-            public void onCancelled(DatabaseError databaseError) {
-            }
-        });
+
 //        DatabaseReference usersRef = ref.child("users");
         databaseReference = FirebaseDatabase.getInstance().getReference();
         //스위치의 체크 이벤트를 위한 리스너 등록
@@ -84,17 +77,13 @@ public class setting_fragment extends Fragment{
                 if (isChecked){
                     Map<String, Object> userUpdates = new HashMap<>();
                     userUpdates.put("/User/"+current_uid+"/LoseState", "True");
-
                     databaseReference.updateChildren(userUpdates);
-
                 }
                 else {
                     Map<String, Object> userUpdates = new HashMap<>();
                     userUpdates.put("/User/"+current_uid+"/LoseState", "False");
                     databaseReference.updateChildren(userUpdates);
                 }
-
-
             }
 
         });
